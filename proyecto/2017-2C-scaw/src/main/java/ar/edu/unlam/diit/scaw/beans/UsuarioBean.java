94ba9f302d62c5699f03cf4baec847b6f72f6c95
José <darius05g4@hotmail.com>
2017-09-17 15:19:43 -0300

    Modificaciones y correcciones

--- proyecto/2017-2C-scaw/src/main/java/ar/edu/unlam/diit/scaw/beans/UsuarioBean.java
+++ proyecto/2017-2C-scaw/src/main/java/ar/edu/unlam/diit/scaw/beans/UsuarioBean.java
@@ -27,14 +27,14 @@ public class UsuarioBean implements Serializable {
 	private String descripcion = null;
 	private Integer idRol = null;
 	private String rolDescripcion = null;
-	
+
 	UsuarioService service;
-	
+
 	public UsuarioBean() {
 		super();
 		service = (UsuarioService) new UsuarioServiceImpl();
 	}
-	
+
 	public UsuarioBean(String eMail, String contraseña, Integer id, String apellido, String nombre, Integer idEstadoUsuario, String descripcion, Integer idRol, String rolDescripcion) {
 		super();
 		this.eMail = eMail;
@@ -49,111 +49,9 @@ public class UsuarioBean implements Serializable {
 		this.idRol = idRol;
 		this.rolDescripcion = rolDescripcion;
 	}
-	
-	public String save() {
-		
-		Usuario person = buildUsuario();
-		
-		service.save(person);
-		
-		return "welcome";
-	}
-	
-	//Listado de todos los usuarios
-	public List<Usuario> getFindAll() {
-		List<Usuario> list = service.findAll();
-		return list;
-	}
-	
-	public String login(){
-		
-		Usuario usuario = new Usuario();
-		usuario.setEmail(this.eMail);
-		usuario.setContraseña(this.contraseña);	
-		usuario.setIdEstadoUsuario(this.idEstadoUsuario);
-		usuario.setDescripcion(this.descripcion);
-		usuario.setIdRol(this.idRol);
-		usuario.setRolDescripcion(this.rolDescripcion);	
-		
-		Usuario logueado = service.login(usuario);		
-		if(logueado!=null && logueado.getIdEstadoUsuario().equals(2)) 
-		{
-			//admin
-			if(logueado.getIdRol().equals(1)) 
-			{
-				return "welcome";
-			}	
-			//docentes
-			else if(logueado.getIdRol().equals(2)) 
-			{
-				return "welcomeDocente";
-			}
-			//alumnos
-			else if(logueado.getIdRol().equals(3)) 
-			{
-				return "welcomeAlumno";
-			}
-			else
-			{
-				return "error";
-			}
-		}
-		else
-		{
-			return "error";
-		}
-		
-	}
 
-	private Usuario buildUsuario() {
-		Usuario person = new Usuario();
-		
-		person.setEmail(this.eMail);
-		person.setContraseña(contraseña);
-		person.setId(this.id);
-		person.setApellido(this.apellido);
-		person.setNombre(this.nombre);
-		person.setIdEstadoUsuario(this.idEstadoUsuario);
-		person.setDescripcion(this.descripcion);
-		person.setIdRol(this.idRol);
-		
-		return person;
-	}
-	
-	//Listado de los usuarios pendientes de habilitacion
-	public List<Usuario> getPendientes() {
-		List<Usuario> list = service.pendientes();
-		return list;
-	}
-	
-	//Aceptar pendiente
-	public String aceptar() {
-			
-			Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-			String id2 = params.get("idParamAceptar");
-			int id = Integer.parseInt(id2);
-	
-			service.aceptar(id);
-			return "pendientes";
-		}
-	
-	//Rechazar pendiente
-	public String rechazar() {
-			
-			Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-			String id2 = params.get("idParamRechazar");
-			int id = Integer.parseInt(id2);
-	
-			service.rechazar(id);
-			return "pendientes";
-		}
-	
-	//Listado de docentes
-		public List<Usuario> getDocentes() {
-			List<Usuario> list = service.docentes();
-			return list;
-		}
-	
+
+
 	public String getEmail() {
 		return eMail;
 	}
@@ -201,7 +99,7 @@ public class UsuarioBean implements Serializable {
 	public void setService(UsuarioService service) {
 		this.service = service;
 	}
-	
+
 	public Integer getIdEstadoUsuario() {
 		return idEstadoUsuario;
 	}
@@ -209,7 +107,7 @@ public class UsuarioBean implements Serializable {
 	public void setIdEstadoUsuario(Integer idEstadoUsuario) {
 		this.idEstadoUsuario = idEstadoUsuario;
 	}
-	
+
 	public String getDescripcion() {
 		return descripcion;
 	}
@@ -217,7 +115,7 @@ public class UsuarioBean implements Serializable {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
+
 	public Integer getIdRol() {
 		return idRol;
 	}
@@ -225,9 +123,109 @@ public class UsuarioBean implements Serializable {
 	public void setIdRol(Integer idRol) {
 		this.idRol = idRol;
 	}
+	
+	public String save() {
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+		Usuario person = buildUsuario();
+
+		service.save(person);
+
+		return "welcome";
+	}
+
+	//Listado de todos los usuarios
+	public List<Usuario> getFindAll() {
+		List<Usuario> list = service.findAll();
+		return list;
+	}
+
+	public String login(){
+
+		Usuario usuario = new Usuario();
+		usuario.setEmail(this.eMail);
+		usuario.setContraseña(this.contraseña);	
+		usuario.setIdEstadoUsuario(this.idEstadoUsuario);
+		usuario.setDescripcion(this.descripcion);
+		usuario.setIdRol(this.idRol);
+		usuario.setRolDescripcion(this.rolDescripcion);	
+
+		Usuario logueado = service.login(usuario);		
+		if(logueado!=null && logueado.getIdEstadoUsuario().equals(2)) 
+		{
+			//admin
+			if(logueado.getIdRol().equals(1)) 
+			{
+				return "welcome";
+			}	
+			//docentes
+			else if(logueado.getIdRol().equals(2)) 
+			{
+				return "welcomeDocente";
+			}
+			//alumnos
+			else if(logueado.getIdRol().equals(3)) 
+			{
+				return "welcomeAlumno";
+			}
+			else
+			{
+				return "error";
+			}
+		}
+		else
+		{
+			return "error";
+		}
+
+	}
+
+	private Usuario buildUsuario() {
+		Usuario person = new Usuario();
+
+		person.setEmail(this.eMail);
+		person.setContraseña(contraseña);
+		person.setId(this.id);
+		person.setApellido(this.apellido);
+		person.setNombre(this.nombre);
+		person.setIdEstadoUsuario(this.idEstadoUsuario);
+		person.setDescripcion(this.descripcion);
+		person.setIdRol(this.idRol);
+
+		return person;
+	}
+
+	//Listado de los usuarios pendientes de habilitacion
+	public List<Usuario> getPendientes() {
+		List<Usuario> list = service.pendientes();
+		return list;
+	}
+
+	//Aceptar pendiente
+	public String aceptar() {
+
+		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String id2 = params.get("idParamAceptar");
+		int id = Integer.parseInt(id2);
+
+		service.aceptar(id);
+		return "pendientes";
+	}
+
+	//Rechazar pendiente
+	public String rechazar() {
+
+		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String id2 = params.get("idParamRechazar");
+		int id = Integer.parseInt(id2);
+
+		service.rechazar(id);
+		return "pendientes";
+	}
+
+	//Listado de docentes
+	public List<Usuario> getDocentes() {
+		List<Usuario> list = service.docentes();
+		return list;
 	}
 
 }
