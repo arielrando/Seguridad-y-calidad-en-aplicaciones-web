358c386a3d51456de7b1a37eb2085849ca32c957
Guinovart, Jose A. <jose.a.guinovart@accenture.com>
2017-09-29 09:56:01 -0300

    Revertidos los cambios

--- proyecto/2017-2C-scaw/src/main/java/ar/edu/unlam/diit/scaw/beans/ExamenBean.java
+++ proyecto/2017-2C-scaw/src/main/java/ar/edu/unlam/diit/scaw/beans/ExamenBean.java
@@ -31,14 +31,14 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	private Integer idPregunta = null;
 	private String respuesta = null;
 	private Integer idTipoRespuesta = null;
-	
+
 	ExamenService service;
-	
+
 	public ExamenBean() {
 		super();
 		service = (ExamenService) new ExamenServiceImpl();
 	}
-	
+
 	public ExamenBean(Integer id, String nombre, String nombreMateria, Integer idMateria, Integer idEstadoExamen, String pregunta,
 			Integer id_pregunta, Integer id_respuesta, Integer idPregunta, String respuesta, Integer idTipoRespuesta) {
 		super();
@@ -53,218 +53,218 @@ public class ExamenBean<ModelAndView> implements Serializable {
 		this.respuesta = respuesta;
 		this.idTipoRespuesta = idTipoRespuesta;
 	}
-	
+
 	public String save() {
-		
+
 		Examen examen = buildExamen();
-		
+
 		service.save(examen);
-		
+
 		return "examenes";
 	}
-	
+
 	//Listado de todos los examenes
 	public List<Examen> getExamenes() {
 		List<Examen> list = service.examenes();
 		return list;
 	}
-	
-	
+
+
 	private Examen buildExamen() {
 		Examen examen = new Examen();
-		
+
 		examen.setId(this.id);
 		examen.setNombre(this.nombre);
 		examen.setIdMateria(this.idMateria);
 		examen.setIdEstadoExamen(this.idEstadoExamen);
-		
+
 		return examen;
 	}
-	
+
 	//Listado de los exámenes pendientes de habilitación
-		public List<Examen> getPendientes() {
-			List<Examen> list = service.pendientes();
-			return list;
-		}
-		
-		//Listado de los exámenes pendientes de habilitación
-		public List<Examen> getHabilitados() {
-			List<Examen> list = service.habilitados();
-			return list;
-		}
-		
-		
-		//Listado de los exámenes habilitados para alumnos
-		public List<Examen> getHabilitadosPorId() {
-			List<Examen> list = service.habilitadosPorId(data);
-			return list;
-		}
-		
-		//Listado de los exámenes y preguntas por id
-		public List<Examen> getPreguntasPorId() {
-			List<Examen> list = service.preguntasPorId(data_examen);
-			return list;
-		}
-		
-		//Listado de los exámenes y preguntas por id
-		public List<Examen> getRespuestasPorId() {
-			List<Examen> list = service.respuestasPorId(data_pregunta);
-			return list;
-		}
-		
-		//Aceptar pendiente
-		public String aceptarExamen() {
-			
-			Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-			String id2 = params.get("idParamAceptarExamen");
-			List<Examen> list = service.preguntasPorId(id2);
-			
-			if (list.size()>0)
-			{int id = Integer.parseInt(id2);
-	
-			service.aceptarExamen(id);
-			return "examenesPendientes";}
-			else
-			{return "examenes";}
-			
-			
-		}
-		
-		//Rechazar pendiente
-		public String rechazarExamen() {
-				
-				Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-				String id2 = params.get("idParamRechazarExamen");
-				int id = Integer.parseInt(id2);
-		
-				service.rechazarExamen(id);
-				return "examenesPendientes";
-			}
-		
-		//Borrar Examen
-		public String borrarExamen() {
-				
-				Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-				String id2 = params.get("idParamBorrarExamen");
-				int id = Integer.parseInt(id2);
-		
-				service.borrarExamen(id);
-				return "examenes";
-			}
-		
-		
-		// PASAR POR PARÁMETROS
-		
-		public String data = "1";
-		
-		public String getData() {
-		      return data;
-		   }
-		
-		public void setData(String data) {
-		      this.data = data;
-		   }
-		
-		//id para examen
-		public String data_examen = "1";
-		
-		public String getData_examen() {
-		      return data_examen;
-		   }
-		
-		public void setData_examen(String data_examen) {
-		      this.data_examen = data_examen;
-		   }
-		
-		//id para pregunta
-		public String data_pregunta = "1";
-		
-		public String getData_pregunta() {
-		      return data_pregunta;
-		   }
-		
-		public void setData_pregunta(String data_pregunta) {
-		      this.data_pregunta = data_pregunta;
-		   }
-		
-		public String realizarExamenAlumnos(){
-		      FacesContext fc = FacesContext.getCurrentInstance();
-		      Map<String,String> params = 
-		      fc.getExternalContext().getRequestParameterMap();
-		      data =  params.get("id"); 
-		      return "realizarExamenAlumnos";
-		   }
-		
-		public String cargarPreguntasPorIdExamen(){
-		      FacesContext fc = FacesContext.getCurrentInstance();
-		      Map<String,String> params = 
-		      fc.getExternalContext().getRequestParameterMap();
-		      data_examen =  params.get("id"); 
-		      return "preguntas";
-		   }
-		
-		public String cargarRespuestasPorIdPregunta(){
-		      FacesContext fc = FacesContext.getCurrentInstance();
-		      Map<String,String> params = 
-		      fc.getExternalContext().getRequestParameterMap();
-		      data_pregunta =  params.get("id_pregunta"); 
-		      return "respuestas";
-		   }
-		//FIN PASAR POR PARÁMETROS
-	
-	//guarda las preguntas del examen	
-	public String savePreguntas() {
-		
-		Examen examen = buildPreguntas();
-		
-		service.savePreguntas(examen, data_examen);
-		
+	public List<Examen> getPendientes() {
+		List<Examen> list = service.pendientes();
+		return list;
+	}
+
+	//Listado de los exámenes pendientes de habilitación
+	public List<Examen> getHabilitados() {
+		List<Examen> list = service.habilitados();
+		return list;
+	}
+
+
+	//Listado de los exámenes habilitados para alumnos
+	public List<Examen> getHabilitadosPorId() {
+		List<Examen> list = service.habilitadosPorId(data);
+		return list;
+	}
+
+	//Listado de los exámenes y preguntas por id
+	public List<Examen> getPreguntasPorId() {
+		List<Examen> list = service.preguntasPorId(data_examen);
+		return list;
+	}
+
+	//Listado de los exámenes y preguntas por id
+	public List<Examen> getRespuestasPorId() {
+		List<Examen> list = service.respuestasPorId(data_pregunta);
+		return list;
+	}
+
+	//Aceptar pendiente
+	public String aceptarExamen() {
+
+		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String id2 = params.get("idParamAceptarExamen");
+		List<Examen> list = service.preguntasPorId(id2);
+
+		if (list.size()>0)
+		{int id = Integer.parseInt(id2);
+
+		service.aceptarExamen(id);
+		return "examenesPendientes";}
+		else
+		{return "examenes";}
+
+
+	}
+
+	//Rechazar pendiente
+	public String rechazarExamen() {
+
+		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String id2 = params.get("idParamRechazarExamen");
+		int id = Integer.parseInt(id2);
+
+		service.rechazarExamen(id);
+		return "examenesPendientes";
+	}
+
+	//Borrar Examen
+	public String borrarExamen() {
+
+		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String id2 = params.get("idParamBorrarExamen");
+		int id = Integer.parseInt(id2);
+
+		service.borrarExamen(id);
+		return "examenes";
+	}
+
+
+	// PASAR POR PARÁMETROS
+
+	public String data = "1";
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	//id para examen
+	public String data_examen = "1";
+
+	public String getData_examen() {
+		return data_examen;
+	}
+
+	public void setData_examen(String data_examen) {
+		this.data_examen = data_examen;
+	}
+
+	//id para pregunta
+	public String data_pregunta = "1";
+
+	public String getData_pregunta() {
+		return data_pregunta;
+	}
+
+	public void setData_pregunta(String data_pregunta) {
+		this.data_pregunta = data_pregunta;
+	}
+
+	public String realizarExamenAlumnos(){
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String,String> params = 
+				fc.getExternalContext().getRequestParameterMap();
+		data =  params.get("id"); 
+		return "realizarExamenAlumnos";
+	}
+
+	public String cargarPreguntasPorIdExamen(){
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String,String> params = 
+				fc.getExternalContext().getRequestParameterMap();
+		data_examen =  params.get("id"); 
 		return "preguntas";
 	}
-	
+
+	public String cargarRespuestasPorIdPregunta(){
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String,String> params = 
+				fc.getExternalContext().getRequestParameterMap();
+		data_pregunta =  params.get("id_pregunta"); 
+		return "respuestas";
+	}
+	//FIN PASAR POR PARÁMETROS
+
+	//guarda las preguntas del examen	
+	public String savePreguntas() {
+
+		Examen examen = buildPreguntas();
+
+		service.savePreguntas(examen, data_examen);
+
+		return "preguntas";
+	}
+
 	private Examen buildPreguntas() {
 		Examen examen = new Examen();
-		
+
 		examen.setId_pregunta(this.id_pregunta);
 		examen.setIdExamen(this.idExamen);
 		examen.setPregunta(this.pregunta);
-		
+
 		return examen;
 	}
-	
+
 	//Borrar Pregunta
 	public String borrarPregunta() {
-			
-			Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-			String id2 = params.get("idParamBorrarPregunta");
-			int id = Integer.parseInt(id2);
-	
-			service.borrarPregunta(id);
-			return "preguntas";
-		}
-	
+
+		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String id2 = params.get("idParamBorrarPregunta");
+		int id = Integer.parseInt(id2);
+
+		service.borrarPregunta(id);
+		return "preguntas";
+	}
+
 	//guarda las respuestas de la pregunta
-		public String saveRespuestas() {
-			
-			Examen examen = buildRespuestas();
-			
-			service.saveRespuestas(examen, data_pregunta);
-			
-			return "respuestas";
-		}
-		
-		private Examen buildRespuestas() {
-			Examen examen = new Examen();
-			
-			examen.setId_respuesta(this.id_respuesta);
-			examen.setIdPregunta(this.idPregunta);
-			examen.setRespuesta(this.respuesta);
-			examen.setIdTipoRespuesta(this.idTipoRespuesta);
-			
-			return examen;
-		}
-		
-		
+	public String saveRespuestas() {
+
+		Examen examen = buildRespuestas();
+
+		service.saveRespuestas(examen, data_pregunta);
+
+		return "respuestas";
+	}
+
+	private Examen buildRespuestas() {
+		Examen examen = new Examen();
+
+		examen.setId_respuesta(this.id_respuesta);
+		examen.setIdPregunta(this.idPregunta);
+		examen.setRespuesta(this.respuesta);
+		examen.setIdTipoRespuesta(this.idTipoRespuesta);
+
+		return examen;
+	}
+
+
 	public Integer getId() {
 		return id;
 	}
@@ -280,7 +280,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getNombreMateria() {
 		return nombreMateria;
 	}
@@ -288,7 +288,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setNombreMateria(String nombreMateria) {
 		this.nombreMateria = nombreMateria;
 	}
-	
+
 
 	public ExamenService getService() {
 		return service;
@@ -297,7 +297,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setService(ExamenService service) {
 		this.service = service;
 	}
-	
+
 	public Integer getIdMateria() {
 		return idMateria;
 	}
@@ -305,7 +305,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setIdMateria(Integer idMateria) {
 		this.idMateria = idMateria;
 	}
-	
+
 	public Integer getIdEstadoExamen() {
 		return idEstadoExamen;
 	}
@@ -313,7 +313,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setIdEstadoExamen(Integer idEstadoExamen) {
 		this.idEstadoExamen = idEstadoExamen;
 	}
-	
+
 	public String getPregunta() {
 		return pregunta;
 	}
@@ -321,7 +321,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setPregunta(String pregunta) {
 		this.pregunta = pregunta;
 	}
-	
+
 	public Integer getIdExamen() {
 		return idExamen;
 	}
@@ -329,7 +329,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setIdExamen(Integer idExamen) {
 		this.idExamen = idExamen;
 	}
-	
+
 	public Integer getId_pregunta() {
 		return id_pregunta;
 	}
@@ -337,7 +337,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setId_pregunta(Integer id_pregunta) {
 		this.id_pregunta = id_pregunta;
 	}
-	
+
 	public Integer getId_respuesta() {
 		return id_respuesta;
 	}
@@ -345,7 +345,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setId_respuesta(Integer id_respuesta) {
 		this.id_respuesta = id_respuesta;
 	}
-	
+
 	public Integer getIdPregunta() {
 		return idPregunta;
 	}
@@ -353,7 +353,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setIdPregunta(Integer idPregunta) {
 		this.idPregunta = idPregunta;
 	}
-	
+
 	public String getRespuesta() {
 		return respuesta;
 	}
@@ -361,7 +361,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setRespuesta(String respuesta) {
 		this.respuesta = respuesta;
 	}
-	
+
 	public Integer getIdTipoRespuesta() {
 		return idTipoRespuesta;
 	}
@@ -369,7 +369,7 @@ public class ExamenBean<ModelAndView> implements Serializable {
 	public void setIdTipoRespuesta(Integer idTipoRespuesta) {
 		this.idTipoRespuesta = idTipoRespuesta;
 	}
-	
+
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
